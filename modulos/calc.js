02549f027060f122e383487d18f5ad2686e51c14
André Luís Tavares Mossignato <andreluis.mossignato@gmail.com>
2025-04-16 18:03:59 -0300

    Módulos

---
+++ modulos/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./somar")
+var SubFunc = require("./subtrair")
+var MultiFunc = require("./multiplicar")
+var DivFunc = require("./dividir")
+
+console.log(SomaFunc(1 , 2))
+console.log(SubFunc(10 , 5))
+console.log(MultiFunc(100 , 10))
+console.log(DivFunc(20 , 2))
